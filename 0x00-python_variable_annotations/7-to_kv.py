c85454397010eeac8707722056c14105f1bb4954
kidus-t-w <kidusk69@gmail.com>
2024-04-18 19:06:02 +0300

    Fixed Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,11 +8,10 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
 
     """
-        Retuns a tuple with a string and a float
+        Returns a tuple with a string and a float
 
         Args:
             k (str): First element of the tuple
             v (float or int): Seconde element to be squared
     """
-    t: Tuple[str, float] = {k, float(v**2)}
-    return t
+    return (k, float(v**2))
