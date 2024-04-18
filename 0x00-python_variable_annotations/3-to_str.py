cfddff4e1c0f2c15788f5678e9653adf66395e40
kidus-t-w <kidusk69@gmail.com>
2024-04-18 16:18:08 +0300

    Added Task

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+3-to_str.py
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float
+
+    Args:
+        n (float): Float to be represented as string
+    """
+
+    return str(n)
