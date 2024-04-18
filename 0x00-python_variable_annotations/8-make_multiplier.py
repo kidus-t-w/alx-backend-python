536be28fecc95f7195435f8c34ff41cd1c7a9c3a
kidus-t-w <kidusk69@gmail.com>
2024-04-18 19:26:24 +0300

    Fixed Task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,15 +2,14 @@
 """
 8-make_multiplier.py
 """
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> function:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Returns a function to be multiplied
 
     Args:
         a (float): float to be multiplied.
     """
-    def mul(a: float) -> float:
-        return a * multiplier
-    return multiplier
+    return lambda x: x * multiplier
