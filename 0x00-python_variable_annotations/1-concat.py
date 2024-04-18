aad8ba45d35ab56f982b447767c316d3642f826a
kidus-t-w <kidusk69@gmail.com>
2024-04-18 16:01:15 +0300

    Fixed Task

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,4 +5,10 @@
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Returns a concatenated string
+    Args:
+        str1: First string
+        str2: Second string
+    """
     return str1 + str2
