ff5e2327e6c6c3aea5c194b1574e6110d0665524
kidus-t-w <kidusk69@gmail.com>
2024-04-18 16:28:33 +0300

    Added Task

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+5-sum_list.py
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns the sum of floats in a list
+
+    Args:
+        input_list (list): list of floats to be added
+    """
+    sum = 0
+    for x in input_list:
+        sum += x
+
+    return sum
