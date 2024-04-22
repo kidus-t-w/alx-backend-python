5017de20cd42162905357eb834a7bd3971262817
kidus-t-w <kidusk69@gmail.com>
2024-04-22 12:11:29 +0300

    Fixed Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,14 +2,15 @@
 """
 1-concurrent_coroutines.py
 """
+import asyncio
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Returns the delay time
+    spawn wait_random n times with the specified max_delay.
     """
     wait_list: List = []
     sort_list: List = []
