105fadb6bbb6299f83b0793e1e9c7a45cc6f666b
kidus-t-w <kidusk69@gmail.com>
2024-04-22 10:50:13 +0300

    Fixed pycodestyle for 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,14 +6,14 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
     """
     Waits for a random delay between 0 and max_delay
-    Returns random delay 
+    Returns random delay
     Args
         max_delay (int): max delay second
     """
     random_number = random.uniform(0, max_delay)
-    
+
     await asyncio.sleep(random_number)
     return random_number
