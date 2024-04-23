27b604403a40f1742215ff7405da5c2298cca31d
kidus-t-w <kidusk69@gmail.com>
2024-04-23 22:26:56 +0300

    Fixed Task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""
-0-async_generator.py
-"""
-import asyncio
-import random
-from typing import AsyncGenerator
+
+""" Async Comprehensions """
+
+from asyncio import sleep
+from random import uniform
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    Generates 10 random numbers and yields them.
-    """
-    for i in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator which yield random number"""
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
