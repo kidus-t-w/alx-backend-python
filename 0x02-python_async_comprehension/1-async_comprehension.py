6535809fa713084edf828c006ae1cfdfe0fb2095
kidus-t-w <kidusk69@gmail.com>
2024-04-23 22:29:42 +0300

    Fixed Task

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,8 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """
+    Async Comprehension
+    """
     list_l = [i async for i in async_generator()]
     return list_l
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,6 +2,7 @@
 """
 Async Comprehensions
 """
+
 import time
 import asyncio
 
@@ -11,7 +12,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Run time for four parallel comprehensions"""
-
     start = time.time()
     task = [async_comprehension() for i in range(4)]
     await asyncio.gather(*task)
