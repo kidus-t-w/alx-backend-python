49e49a7f668256885751ce840576af45a63e01b1
kidus-t-w <kidusk69@gmail.com>
2024-05-13 10:59:00 +0300

    Created a test class for nested map functions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 import utils
 import unittest
+from unittest import assertEqual, TestCase
+from parameterized import parameterized
 
-class TestAccessNestedMap(unittest.TestCase):
-    pass
+
+class TestAccessNestedMap(TestCase):
+    """
+    Test class for testing the function 'access_nested_map'
+    which is used to access a nested map with a key list.
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test accessing nested map with different paths"""
+        result = utils.access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
