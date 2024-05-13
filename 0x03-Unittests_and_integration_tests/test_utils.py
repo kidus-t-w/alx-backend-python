5a5d85a23f8e510f359ef192ba87d91497bd635e
kidus-t-w <kidusk69@gmail.com>
2024-05-13 11:22:50 +0300

    Modified library import

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
+
     def test_access_nested_map(self, nested_map, path, expected):
         """Test accessing nested map with different paths"""
         result = utils.access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+if __name__ == "__main__":
+    unittest.main()
